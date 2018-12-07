cd94005f10197009ad93ccdfc776e9db4b50869b
Bereket k <kelemub@gmail.com>
2018-12-07 10:21:28 -0500

    First Commit

---
+++ Text-Name-Search/Models/Names.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Text_Name_Search.Models
+{
+    public class Names
+    {
+        [Required(ErrorMessage ="This field is required.")]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string MiddleName { get; set; }
+      
+    }
+}
---
+++ Text-Name-Search/Scripts/Scripts.js
@@ -0,0 +1,33 @@
+﻿function jQueryAjaxPost(form) {
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid()) {
+        var ajaxConfig = {
+            type: "POST",
+            url: form.action,
+            data: new FormData(form),
+            success: function (response) {
+                $("#nameTab").html(response);
+                refreshAddNewTab($(form).attr('data-resetUrl',true))
+            }
+        }
+        if ($(form).attr("enctype") == "multiport/form-data") {
+            ajaxConfig["contentType"] = false;
+            ajaxConfig["processData"] = false;
+        }
+        $.ajax(ajaxConfig);
+    }
+    return false;
+}
+
+function refreshAddNewTab(resetUrl, showViewTab) {
+    ajax({
+        type: 'GET',
+        url: resetUrl,
+        success: function (response) {
+            $("#nameTab").html(response);
+            $('ul.nav.nav-tabs a:eq(1)').html("Add Name");
+            if (showViewTab)
+                $('ul.nav.nav-tabs a:eq(0)');
+        }
+    });
+}
