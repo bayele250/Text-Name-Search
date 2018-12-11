d54aaf3075d577323e19b3f15ff7a4a87dfb1ede
bayele250 <bayele250@gmail.com>
2018-12-11 10:10:37 -0500

    New Update

---
+++ Text-Name-Search/Models/ArticleNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Text_Name_Search.Models
+{
+    public class ArticleNames
+    {
+        public string FullName { get; set; }
+        public int TotalOccurance { get; set; }
+    }
+}
